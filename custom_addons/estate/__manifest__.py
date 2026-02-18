136dc336db3a379389d4a7b3388171faf314ccd3
glassesboyy <aliperwira26@gmail.com>
2026-02-19 00:55:41 +0700

    first commit: introduction, module structure, models & fields

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate',
+    'version': '1.0.0',
+    'summary': 'Modul Estate untuk belajar Odoo 19',
+    'description': 'Modul sederhana untuk manajemen estate.',
+    'author': 'Teguh Surya Zulfikar',
+    'website': 'https://example.com',
+    'category': 'Real Estate',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
