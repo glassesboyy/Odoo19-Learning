a29221abc2ae2810b7d863791d2f6357533d930f
glassesboyy <aliperwira26@gmail.com>
2026-02-19 13:41:52 +0700

    chapter 5: actions & menus

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -11,6 +11,8 @@
     'data': [
         'security/res_groups.xml',
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml',
     ],
     'demo': [
         'demo/demo.xml',
