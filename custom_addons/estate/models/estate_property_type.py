01aed69233cee33f3ab99891a24219ec5fb0abdc
glassesboyy <aliperwira26@gmail.com>
2026-03-03 10:43:15 +0700

    chapter 17: more buttons

--- custom_addons/estate/models/estate_property_type.py
+++ custom_addons/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
@@ -12,3 +12,10 @@ class EstatePropertyType(models.Model):
     sequence = fields.Integer(default=1)
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id', string='Properties')
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id', string='Offers')
+    offer_count = fields.Integer(compute='_compute_offer_count', string='Offer Count')
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
